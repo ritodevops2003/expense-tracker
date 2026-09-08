b436b34316e9a66b0d5eadf321ac64b2cd23ba29
webblacklight <web@blacklightgrp.com>
2026-09-09 01:42:41 +0530

    Add delete and edit for expenses

--- app/expenses/expense-row.tsx
+++ app/expenses/expense-row.tsx
@@ -1,6 +1,7 @@
 // app/expenses/expense-row.tsx — Client Component (needs state + onClick)
 "use client";
 import { useState } from "react";
+import { deleteExpense, updateExpense } from "../lib/actions"
 
 export default function ExpenseRow({
   expense,
@@ -15,13 +16,36 @@ export default function ExpenseRow({
 }) {
   const [paid, setPaid] = useState(false);
 
+  const deleteThisOne = deleteExpense.bind(null, expense.id)
+
+  const [editing, setEditing] = useState(false)
+  const updateThisOne = updateExpense.bind(null, expense.id)
+
+  if (editing){
+    return(
+      <li>
+        <form action={updateThisOne}>
+              <input name="name" defaultValue={expense.name} />
+              <input name="amount" type="number" step="0.01" defaultValue={expense.amount} />
+              <button type="submit">Save Action</button>
+        </form>
+        <button onClick={() => setEditing(false) }>Cancel</button>
+      </li>
+    )
+  }
+
   return (
     <li>
       {expense.name} ({expense.category}) : ${expense.amount} —{" "}
-      {expense.date.toLocaleDateString()}
+      <form action={deleteThisOne}>
+        <button type='submit'>Delete</button>
+      </form>
+      {expense.date.toLocaleDateString('en-GB')}
       <button onClick={() => setPaid(!paid)}>
         {paid ? "Paid ✓" : "Mark as paid"}
       </button>
+
+      <button onClick={() => setEditing(true)}>Edit</button>
     </li>
   );
 }
--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,6 +6,7 @@ import { parseExpense } from "./parse-expense";
 // import { categorize } from "./categorize"
 // import { expenses } from './data'
 import { revalidatePath } from "next/cache";
+import { Prisma } from "@/generated/prisma/client";
 
 export async function addExpense(formData: FormData) {
   const text = formData.get("text") as string;
@@ -18,4 +19,21 @@ export async function addExpense(formData: FormData) {
   await prisma.expense.create({ data: { ...parsed, name: parsed.name, amount: parsed.amount, date: parsed.date ? new Date(parsed.date) : new Date() } });
 
   revalidatePath("/expenses"); // tell Next.js this page's data changed
+}
+
+export async function deleteExpense(id: number) {
+  await prisma.expense.delete({ where: { id } })
+  revalidatePath('/expenses')
+}
+
+export async function updateExpense(id:number, formData: FormData) {
+  const name = formData.get('name') as string
+  const amount = Number(formData.get('amount'))
+
+  await prisma.expense.update({
+    where: { id },
+    data: { name, amount }
+  })
+
+  revalidatePath('/expenses')
 }
